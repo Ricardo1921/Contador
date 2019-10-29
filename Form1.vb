2ed76fafce9c9c5b0baf4129d73047e68568139e
Ricardo1921 <49916582+Ricardo1921@users.noreply.github.com>
2019-10-29 16:21:22 +0000

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Public
+
+End Class
+
+Public Class MyPictureBox
+        Interits Picture
+
+
+        Public Property linha As Integer
+
+        Public Property coluna As Integer
+
+        Public Property minas As Integer
+
+        Public Property bombas As Integer
+        Private Sub clicar(sender As Object, e As EventArgs) Handles 
+            Dim pic As PictureBox
+            pic = sender
+            pic.BackgroundImage = My.Resources.
+            Dim imagem = sender.name
+            Dim quadro = Int(imagem.Replace("b", ""))
+            MsgBox(quadro)
+        End Sub
+    End Class
+
+
+
+
